45f7c2da8e61e1ea29508f399655bd0aeacb96ad
afeiship <1290657123@qq.com>
2017-08-15 15:34:25 +0800

    issue: minify example

--- src/app.server.js
+++ src/app.server.js
@@ -5,43 +5,33 @@ import React, { Component } from 'react';
 
 class App extends Component{
   render(){
-    const {test} = this.props;
     return (
       <main className="iam-from-server" id="root" data-role="app-shell">
-        Hello {test}
+        Loading page....
       </main>
     );
   }
 }
 
-function* renderApp(next) {
-  this.body = yield initAndRender({
-    test:'fei'
-  });
-}
+function * renderApp(next) {
+  const initialState = { test: 'afeiship' };
+  const appShellHtml = renderToString(<App />);
 
-function* initAndRender(props) {
-  const appShellHtml = renderToString(<App {...props} />);
-  return renderIndex(appShellHtml, props);
-}
-
-function renderIndex(inAppShellHtml, inInitialState) {
-  // can also be done with template engine like ejs or jade/pug
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>React SSR</title>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(inInitialState || {})}
-        </script>
-      </head>
-      <body>
-          ${inAppShellHtml}
-          <script type="application/javascript" src="/build/bundle.js"></script>
-      </body>
-    </html>`.trim();
+  this.body = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>React SSR</title>
+          <script>
+            window.__INITIAL_STATE__ = ${JSON.stringify(initialState || {})}
+          </script>
+        </head>
+        <body>
+            ${appShellHtml}
+            <script type="application/javascript" src="/build/bundle.js"></script>
+        </body>
+      </html>`.trim();
 }
 
 module.exports = {
